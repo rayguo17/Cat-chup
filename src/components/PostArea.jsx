9f1de78e7af192f4da5d3b6850ccade141fe7cd2
Caspartai <taikalungcc@gmail.com>
2021-07-15 16:48:41 +0800

    post try to use props

--- src/components/PostArea.jsx
+++ src/components/PostArea.jsx
@@ -4,16 +4,35 @@ import Post from './Post'
 
 
 const PostArea = (props) => {
+
+    const postInfo = 
+    { 
+        id:1,
+        userInfo:
+        {
+            userName:"Username",
+            userIcon_url:"https://image.flaticon.com/icons/png/512/146/146005.png"
+        },
+        content:
+        {
+            caption:"Nice to create a post",
+            text:"It is a good day.",
+            picture:{
+                name:"pic1",
+                data_url:"https://img.huffingtonpost.com/asset/5e0f68ec2500003b1998fb2e.jpeg?cache=YqiWjN9UVt&ops=crop_34_446_5966_3406%2Cscalefit_720_noupscale"
+                    }
+        }, 
+        postTime: "01-07-2020 15:00:00" ,
+        likeNumber: 5,
+        commentsNumber: 1,
+    }
+
     return (
         <div className="postContainer">
 
-            <Post />
+            <Post postInfo={postInfo} />
             <EventPost />
-     
-
-
-
-
+    
         </div>)
 }
 
