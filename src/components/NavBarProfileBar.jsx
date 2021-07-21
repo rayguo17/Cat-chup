df053b3fa1fa4b3e9a58503f8e3b869affb2c2d0
TsungmingIp <tsungmingip@gmail.com>
2021-07-22 02:32:28 +0800

    NavbarActiveChanged

--- src/components/NavBarProfileBar.jsx
+++ src/components/NavBarProfileBar.jsx
@@ -5,42 +5,50 @@ import { useSelector } from 'react-redux';
 import '../stylesheet/navBar.css'
 import { useState } from 'react'
 import Logout from './Logout';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 
 
 const NavBarProfileBar = (props) => {
-
-
     const userStore = useSelector(state => state.userInfoStore)
     const userInfo = userStore.userInfo
-
-    console.log("this is userInfo: ", userInfo)
-
-
+    // console.log("this is userInfo: ", userInfo)
     // let toggle = false;
     const [toggle, setToggle] = useState(false)
-
     const toggleLogout = () => {
-        console.log("helloWorld")
-        setToggle(!toggle)
-
+        // console.log("helloWorld")
+        setToggle((prev) => !prev);
     }
 
-
+    const handleClickAway = () => {
+        setToggle(false);
+    };
 
     return (
-        <footer className="pt-2">
-            <button onClick={toggleLogout} style={{ border: "none", background: "none" }}>
-                <div style={{ display: 'inline-block', width: '50px', height: '50px', backgroundImage: `url(${process.env.REACT_APP_API_SERVER + userInfo.imgPath})`, backgroundSize: "contain", borderRadius: "50px" }}>
-
-                </div>
-                <span style={{ verticalAlign: "20px", marginLeft: "10px" }}>{userInfo.username}</span>
-            </button>
-            {toggle && (
-                <div className="logoutToggle" style={{ position: "absolute", bottom: "60px", left: "100px" }}>
-                    <Logout />
-                </div>
-            )}
-        </footer>)
+        <ClickAwayListener
+            mouseEvent="onMouseDown"
+            touchEvent="onTouchStart"
+            onClickAway={handleClickAway}
+        >
+            <div style={{ width: "110px" }}>
+                <button onClick={toggleLogout} style={{ border: "none", background: "none" }}>
+                    <div style={{
+                        display: 'inline-block', width: '50px',
+                        height: '50px',
+                        backgroundPosition: 'center center',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundImage: `url(${process.env.REACT_APP_API_SERVER + userInfo.imgPath})`,
+                        backgroundSize: "cover", borderRadius: "50px"
+                    }}>
+                    </div>
+                    <span style={{ verticalAlign: "20px", marginLeft: "10px" }}>{userInfo.username}</span>
+                </button>
+                {toggle && (
+                    <div className="logoutToggle" style={{ position: "absolute", bottom: "60px", left: "100px" }}>
+                        <Logout />
+                    </div>
+                )}
+            </div>
+        </ClickAwayListener>)
 }
 
 
