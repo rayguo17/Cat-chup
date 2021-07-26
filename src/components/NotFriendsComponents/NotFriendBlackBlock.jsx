ae027a241953ba509b17dc12d54d10a1648968ce
TsungmingIp <tsungmingip@gmail.com>
2021-07-26 15:28:07 +0800

    fix css

--- src/components/NotFriendsComponents/NotFriendBlackBlock.jsx
+++ src/components/NotFriendsComponents/NotFriendBlackBlock.jsx
@@ -5,7 +5,7 @@ export const NotFriendBlackBlock = (props) => {
     const { pageOwnerName, areFriends } = props
 
     return (
-        <div className='col-9 px-0 row mx-0' style={{position:"absolute", bottom: "0",width:"webkit-fill-available"}}>
+        <div className='col-12 px-0 row mx-0' style={{position:"absolute", bottom: "0"}}>
         <div  style={{
             height: "50vh",
             backgroundColor: "#434343", zIndex: "20"
